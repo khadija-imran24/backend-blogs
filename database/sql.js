aedba63177ce858071176a766321d8422c5bcf70
khadija imran <thankyouparrot23239@gmail.com>
2025-08-05 17:15:11 +0500

    Updated routes and structure in www folder

--- database/sql.js
+++ database/sql.js
@@ -1,34 +1,12 @@
 const mysql = require('mysql');
-let connection;
-function handleDisconnect() {
-  connection = mysql.createConnection({
-  host: 'b2ono8d7tfmzajnhawjd-mysql.services.clever-cloud.com',
-  user: 'urhfqk3pxurx8rqf',
-  password: 'tcNRCGMvyscmba4CPzF4',
-  database: 'b2ono8d7tfmzajnhawjd',
-  port: 3306 // usually 3306, confirm from Clever Cloud panel
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.MYSQL_HOST || 'b2ono8d7tfmzajnhawjd-mysql.services.clever-cloud.com',
+  user: process.env.MYSQL_USER || 'urhfqk3pxurx8rqf',
+  password: process.env.MYSQL_PASSWORD || 'tcNRCGMvyscmba4CPzF4',
+  database: process.env.MYSQL_DATABASE || 'b2ono8d7tfmzajnhawjd',
+  port: process.env.MYSQL_PORT || 3306,
 });
-
-
-  connection.connect(function (err) {
-    if (err) {
-      console.log('Error connecting to MySQL:', err);
-      setTimeout(handleDisconnect, 2000); // retry after 2 sec
-    } else {
-      console.log('Connected to MySQL Database');
-    }
-  });
-
-  connection.on('error', function (err) {
-    console.log('MySQL error', err);
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      handleDisconnect(); // reconnect
-    } else {
-      throw err;
-    }
-  });
-}
-
-handleDisconnect();
-
-module.exports = connection;
+console.log('Database connection pool created');
+module.exports = pool;
